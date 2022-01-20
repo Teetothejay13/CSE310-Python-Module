40970c5893bb78a25e2ece64bbefb3a30ecd30ac
Teetothejay13 <teetothejay13@gmail.com>
2022-01-19 19:31:33 -0700

    completed read, write, create, and display functions

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Overview
 
-{Important!  Do not say in this section that this is college assignment.  Talk about what you are trying to accomplish as a software engineer to further your learning.}
+In creating this program, I am refreshing my knowledge of Python. At the time of creating this, I have not had a chance to use Python in about 6 months, so much of my knowledge is rusty. By creating a program such as this, I can refresh my knowledge, as well as have a good program for learning other languages. As everyone knows, one of the best ways to learn a new programming language is to recreate an old program you already have, but in the new language. I feel like the amount of concepts covered by this program will be a good learning aide for future languages.
 
-{Provide a description the software that you wrote to demonstrate the Python language.}
+In this program, I created a user input loop so that the user did not have to restart the program every time they wanted to select a different option. Among these options, the user can choose to read a file, edit a file, or create a new file. These files will be baking or cooking recipes, stored as JSON files that can be manipulated however the user wants.
 
-{Describe your purpose for writing this software.}
+I am an avid baker, so I came up with this idea of a digital recipe box while I was baking some banana bread (see the recipe included). It will come in handy when I am away from my physical copy of my recipes.
 
 {Provide a link to your YouTube demonstration.  It should be a 4-5 minute demo of the software running and a walkthrough of the code.  Focus should be on sharing what you learned about the language syntax.}
 
@@ -12,13 +12,15 @@
 
 # Development Environment
 
-{Describe the tools that you used to develop the software}
+I used the VS Code editor to develop this program.
 
-{Describe the programming language that you used and any libraries.}
+I used python for this program, and I included the json library. I wanted a way to store recipes, but not as one big block of text. Editing
+a big text file would be annoying, so making each recipe a JSON file I can interact with was much easier and more efficient.
 
 # Useful Websites
 
 * [Stack Overflow JSON Forum](https://stackoverflow.com/questions/32991069/python-checking-for-json-files-and-creating-one-if-needed)
+* [Grepper JSON Dump to File](https://www.codegrepper.com/code-examples/javascript/json+dump+to+file)
 * [Web Site Name](http://url.link.goes.here)
 
 # Future Work
--- recipeBook.py
+++ recipeBook.py
@@ -14,26 +14,129 @@
 
 # we'll need these
 import json
-import os
-import io
 
 
 # main will run the show
 def main():
     # first, we need an input loop the user can quit from
     userInput = ""
-    print("Welcome to your personal recipe book!/n")
+    print("Welcome to your personal recipe book!\n")
     while userInput != "q":
-        print("Please select what you would like to do:/n")
+        print("Open a recipe (o)")
+        print("Edit a recipe (e)")
+        print("Create new recipe (n)")
+        print("Quit (q)\n")
+        userInput = input("Please select what you would like to do: ")
+        # now we can have if/else statements calling the functions for each input
+        # opening the file should be pretty simple, as long as we have safeguards against missing or unreadable files
+        if userInput == "o":
+            fileName = input("Please enter the file name of the recipe you would like to open: ")
+            # here we will open the file, and store the json data into separate lists that can be displayed
+            contents = readFile(fileName)
+            if contents == False:
+                print("There was a problem reading the file. Please try again or choose another recipe.")
+            else:
+                display(contents)
+        # if there is something wrong with the recipe, we want to be able to edit it and fix it
+        elif userInput == "e":
+            pass
+        # we don't want to have to manually create new json files, so we have a function to do that for us
+        elif userInput == "n":
+            newFileName = input("Please enter the name you would like to call the new file: ")
+            createFile(newFileName)
+        elif userInput == "q":
+            print("Thank you for cooking with me!")
 
 # this function will open the file and read it after checking if it's there, prompting to create the file if it isn't
-def openFile(fileName):
-    pass
-
+def readFile(fileName):
+        # if the file exists and is readable, the try will succeed and return the information
+        try:
+            with open(fileName, "r") as json_file:
+                text = json.load(json_file)
+                name = text["Name"]
+                ingredients = text["Ingredients"]
+                other = text["Other Materials"]
+                instructions = text["Instructions"]
+                return name, ingredients, other, instructions
+        # if it isn't, then we will ask if the user wants a new file, and proceed from there
+        except IOError:
+            createNew = input("Either the file is missing, or the recipe is not readable. Would you like to create a new recipe? (y/n): ")
+            # if they want a new file, we ask for the file name and call the createFile() function
+            if createNew == "y":
+                newFileName = input("Please enter the name you would like to call the new file: ")
+                createFile(newFileName)
+            # if they don't want a new file, we just return False so main knows not to display anything
+            else:
+                return False
+        
 # this function will allow the user to edit the recipe and save the changes
 def editRecipe(fileName):
     pass
 
+# this function will create a new recipe, whether called from readFile() or main()
+def createFile(newFileName):
+    # before creating the file, we might as well get the information to put in it first
+    name = input("\nWhat would you like to name the recipe? ")
+    # for ingredients, other materials, and instructions, we will have to make a list and append additional items to it
+    print("Great! Now lets get some ingredients recorded.")
+    ingredients = []
+    addIngredient = ""
+    # we want to keep adding ingredients until the user is done
+    while addIngredient != "n":
+        newIngredient = input("What ingredient would you like to add? Include amounts: ")
+        ingredients.append(newIngredient)
+        addIngredient = input("Would you like to add another ingredient? (y/n): ")
+    # now we need instructions, it will work similarly to the ingredients
+    print("Awesome! Now that we have ingredients, how do we prepare this dish?")
+    instructions = []
+    addInstruction = ""
+    while addInstruction != "n":
+        newInstruction = input("Please type the instruction you would like to add: ")
+        instructions.append(newInstruction)
+        addInstruction = input("Would you like to add another instruction? (y/n): ")
+    # we can't forget other materials like pans or utensils if there are any
+    print("Splendid! What kind of other materials or cooking utensils will we need?")
+    other = []
+    addOther = ""
+    while addOther != "n":
+        newOther = input("Enter the material you would like to add: ")
+        other.append(newOther)
+        addOther = input("Would you like to add another material? (y/n): ")
+
+    # now that we have what we need, we can create the file and write the information in
+    with open(newFileName, "w+") as new_file:
+        content = {
+            "Name": name,
+            "Ingredients": ingredients,
+            "Other Materials": other,
+            "Instructions": instructions
+        }
+        json.dump(content, new_file)
+        
+
 # this function will display the information that the user needs displayed
-def display():
-    pass
+def display(contents):
+    # first we have to sort out the contents
+    name = contents[0]
+    ingredients = contents[1]
+    other = contents[2]
+    instructions = contents[3]
+    print(f"\n{name}\n")
+    print("For this recipe, you will need:")
+    # we can loop through the list of ingredients to print them out
+    for ingredient in ingredients:
+        print(f"\t{ingredient}")
+    print("\nAs well as: ")
+    # same with other materials
+    for material in other:
+        print(f"\t{material}")
+    print("\nInstructions:")
+    # and the same with instructions
+    for instruction in instructions:
+        print(f"\t{instruction}")
+    print()
+    
+
+# We need to include this part otherwise it won't run automatically.
+if __name__ == "__main__":
+  main()
